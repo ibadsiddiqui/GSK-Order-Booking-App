e550a2655322d5c1efcbbf37d425ffdc09679837
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-26 23:14:46 +0500

    --Commit: actions and dispatchers added

---
+++ src/redux/actions/OrdersActions.js
@@ -0,0 +1,41 @@
+export const AddOrderProduct = () => {
+    return {
+
+    }
+}
+
+export const AddOrderQuantity = () => {
+    return {
+
+    }
+}
+
+export const AddOrderDeliveryDate = () => {
+    return {
+
+    }
+}
+
+export const AddOrderGeoLocation = () => {
+    return {
+
+    }
+}
+
+export const AddOrderShopPicture = () => {
+    return {
+
+    }
+}
+
+export const CreateOrder = () => {
+    return {
+
+    }
+}
+
+export const AddOrderToReceivedOrderList = () => {
+    return {
+
+    }
+}
