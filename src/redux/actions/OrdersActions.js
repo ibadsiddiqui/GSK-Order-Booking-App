53121762ba7c37a3aed63445429e913ae5ce1890
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-26 23:32:11 +0500

    --Commit: minimal actions added for orders

--- src/redux/actions/OrdersActions.js
+++ src/redux/actions/OrdersActions.js
@@ -1,41 +1,50 @@
-export const AddOrderProduct = () => {
-    return {
+import { ADD_QUANTITY_FOR_ORDER, ADD_DELIVERY_DATE_FOR_ORDER, ADD_GEOLOCATION_FOR_ORDER, ADD_SHOP_PICTURE_FOR_ORDER, SUBMIT_ORDER, PUSH_ORDER_TO_RECEIVED_LIST, ADD_PRODUCT_FOR_ORDER } from "../types";
 
+export const AddOrderProduct = (productID) => {
+    return {
+        type: ADD_PRODUCT_FOR_ORDER,
+        payload: productID
     }
 }
 
-export const AddOrderQuantity = () => {
+export const AddOrderQuantity = (quantity) => {
     return {
-
+        type: ADD_QUANTITY_FOR_ORDER,
+        payload: quantity,
     }
 }
 
-export const AddOrderDeliveryDate = () => {
+export const AddOrderDeliveryDate = (date) => {
     return {
-
+        type: ADD_DELIVERY_DATE_FOR_ORDER,
+        payload: date,
     }
 }
 
-export const AddOrderGeoLocation = () => {
+export const AddOrderGeoLocation = (data) => {
     return {
-
+        type: ADD_GEOLOCATION_FOR_ORDER,
+        payload: data,
     }
 }
 
-export const AddOrderShopPicture = () => {
+export const AddOrderShopPicture = (image) => {
     return {
-
+        type: ADD_SHOP_PICTURE_FOR_ORDER,
+        payload: image
     }
 }
 
-export const CreateOrder = () => {
+export const CreateOrder = (order) => {
     return {
-
+        type: SUBMIT_ORDER,
+        payload: order
     }
 }
 
-export const AddOrderToReceivedOrderList = () => {
+export const AddOrderToReceivedOrderList = (orderID) => {
     return {
-
+        type: PUSH_ORDER_TO_RECEIVED_LIST,
+        payload: orderID
     }
 }
