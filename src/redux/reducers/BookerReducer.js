10663244c886165297103c2aa7de2667122723f0
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-26 23:14:31 +0500

    --Commit: reducers added

---
+++ src/redux/reducers/BookerReducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+
+}
+
+const BookerReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case value:
+
+            break;
+
+        default:
+            break;
+    }
+}
+
+export default BookerReducer;
---
+++ src/redux/reducers/OrdersReducer.js
@@ -0,0 +1,17 @@
+
+const initialState = {
+
+}
+
+const OrdersReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case value:
+
+            break;
+
+        default:
+            break;
+    }
+}
+
+export default OrdersReducer;
