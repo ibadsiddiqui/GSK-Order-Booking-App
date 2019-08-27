668332ca24a2dce4a66fac0cd6c22e704869b132
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-28 04:11:56 +0500

    --Commit: store added to parent component

--- App.js
+++ App.js
@@ -4,11 +4,11 @@ import * as Font from 'expo-font';
 import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 
 import AppNavigator from './src/navigation/AppNavigator';
-
-// const store = configureStore();
+import configureStore from './src/redux/store';
+const store = configureStore();
 
 
 export default function App(props) {
@@ -16,40 +16,39 @@ export default function App(props) {
 
   if (!isLoadingComplete && !props.skipLoadingScreen) {
     return (
-      // <Provider store={store.store}>
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
-      // </Provider>
+      <Provider store={store.store}>
+        <AppLoading
+          startAsync={loadResourcesAsync}
+          onError={handleLoadingError}
+          onFinish={() => handleFinishLoading(setLoadingComplete)}
+        />
+      </Provider>
     );
   } else {
     return (
-      // <Provider store={store.store}>
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
-      // </Provider>
+      <Provider store={store.store}>
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </View>
+      </Provider>
     );
   }
 }
 
 async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./src/assets/images/robot-dev.png'),
-      require('./src/assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./src/assets/fonts/SpaceMono-Regular.ttf'),
-    }),
+  await Asset.loadAsync([
+    require('./src/assets/images/robot-dev.png'),
+    require('./src/assets/images/robot-prod.png'),
   ]);
+  // await Font.loadAsync({
+    // This is the font that we are using for our tab bar
+    // ...Ionicons.font,
+    // We include SpaceMono because we use it in HomeScreen.js. Feel free to
+    // remove this if you are not using it in your app
+    // 'space-mono': require('./src/assets/fonts/SpaceMono-Regular.ttf'),
+  // });
+  // return true
 }
 
 function handleLoadingError(error) {
