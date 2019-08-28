89d1893d249dbf7f67f95abb824101156133c2c1
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-29 00:03:25 +0500

    --Commit: data cached

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, FontAwesome, Entypo, Feather, MaterialCommunityIcons } from '@expo/vector-icons';
 import { AppLoading } from 'expo';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
@@ -46,6 +46,10 @@ async function loadResourcesAsync() {
   await Font.loadAsync({
     // This is the font that we are using for our tab bar
     ...Ionicons.font,
+    ...FontAwesome.font,
+    ...Entypo.font,
+    ...Feather.font,
+    ...MaterialCommunityIcons.font,
     // We include SpaceMono because we use it in HomeScreen.js. Feel free to
     // remove this if you are not using it in your app
     'space-mono': require('./src/assets/fonts/SpaceMono-Regular.ttf'),
