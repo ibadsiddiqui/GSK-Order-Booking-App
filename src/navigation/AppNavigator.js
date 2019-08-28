1f35e037e5675d6c9f396dc5d3ecba618864b14a
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-28 17:49:02 +0500

    --Commit: add product to order screen added

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,13 +1,14 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import ProductInfo from '../screens/ProductInfo';
+import AddProductToOrderScreen from './../screens/AddProductToOder';
 import MainTabNavigator from './MainTabNavigator';
-
 
 export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
     Main: MainTabNavigator,
-    ProductInfo: ProductInfo
+    ProductInfo: ProductInfo,
+    AddProductToOrder: AddProductToOrderScreen
   })
 );
