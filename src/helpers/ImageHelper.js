7917adb9bebcd7334f6d4fe84a7a599ecafc16fd
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-28 15:16:12 +0500

    --Commit image picker added

---
+++ src/helpers/ImageHelper.js
@@ -0,0 +1,59 @@
+import { ImagePicker, MediaLibrary } from "expo";
+import { Platform } from 'react-native';
+import { takeCameraPermissionAsync, takeCameraRollPermissionAsync } from "../constants/Permissions";
+
+export async function UploadImage(uploadType) {
+    const cameraPerm = await takeCameraPermissionAsync()
+    const cameraRollPerm = await takeCameraRollPermissionAsync();
+    let result;
+    if (cameraPerm === "granted" && cameraRollPerm === "granted") {
+        switch (uploadType) {
+            case "takephoto":
+                result = await _openCamera(Platform.OS);
+                if (Platform.OS === "android")
+                    if (typeof result.uri === "string")
+                        await MediaLibrary.createAssetAsync(result.uri);
+                    else return { cancelled: true }
+                break;
+            case "uploadphoto":
+                result = await _openMediaLibrary()
+                break;
+            default:
+                break;
+        }
+        return result;
+    } else return { cancelled: true }
+}
+
+
+async function _openCamera(OS) {
+    switch (OS) {
+        case "android":
+            return await ImagePicker.launchCameraAsync({
+                allowsEditing: true,
+                base64: false,
+                exif: true
+            });
+        default:
+            const result = await ImagePicker.launchCameraAsync({
+                base64: false,
+                exif: true
+            });
+            await MediaLibrary.createAssetAsync(result.uri);
+            return await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                mediaTypes: "Images",
+                base64: false,
+                exif: true,
+            });
+    }
+}
+
+async function _openMediaLibrary() {
+    return await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        mediaTypes: "Images",
+        base64: false,
+        exif: true,
+    });
+}
