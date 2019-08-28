5075109551018e19d4c57af8724f9beb27e4d2ca
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-28 13:09:13 +0500

    --Commit: props style added

--- src/components/TabBarIcon.js
+++ src/components/TabBarIcon.js
@@ -1,14 +1,14 @@
-import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
-
+import React from 'react';
 import Colors from '../constants/Colors';
+
 
 export default function TabBarIcon(props) {
   return (
     <Ionicons
       name={props.name}
       size={26}
-      style={{ marginBottom: -3 }}
+      style={{ marginBottom: -3, ...props.style }}
       color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
     />
   );
