63636116c015dede0bbba56e50ae4e683ce7ffc0
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-28 13:14:33 +0500

    --Commit: Products list screen updated

--- src/screens/Dashboard/ProductsTab/ProductsList/index.js
+++ src/screens/Dashboard/ProductsTab/ProductsList/index.js
@@ -1,47 +1,49 @@
-import { FontAwesome } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 import React from 'react';
 import { Dimensions, FlatList, Text, TouchableOpacity, View } from 'react-native';
 import { _keyExtractor } from '../../../../commons/utils';
-import Colors from '../../../../constants/Colors';
+import TabBarIcon from '../../../../components/TabBarIcon';
 import Layout from '../../../../constants/Layout';
 import ProductLists from '../../../../constants/ProductsList';
 const { width } = Dimensions.get('window')
 
-export default function ProductListScreen() {
-    return (
-        <View style={styles.container}>
-            <View style={Layout.tableRow}>
-                <View style={[Layout.tableCell, { alignItems: 'center', paddingTop: 40 }]}>
-                    <Text style={{ textAlign: 'center', fontSize: 22, fontWeight: 'bold', color: Colors.tintColor }}>Products List by GSK</Text>
+export default class ProductListScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Products List by GSK',
+    };
+
+    onPress(productInfo) {
+        return this.props.navigation.navigate('ProductInfo', { productInfo: productInfo })
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={{ flex: 5 }}>
+                    <FlatList
+                        style={{ width }}
+                        data={ProductLists}
+                        keyExtractor={_keyExtractor}
+                        renderItem={({ item, index }) =>
+                            <TouchableOpacity style={{ flexDirection: 'row', width, margin: 10 }}
+                                onPress={() => this.onPress(item)}
+                            >
+                                <View style={{ marginHorizontal: 10 }} >
+                                    <Entypo name="dot-single" size={20} color="#0078ff" />
+                                </View>
+                                <Text style={{ textAlign: 'left', width: 220 }}>{item.name}</Text>
+                                <View style={{ alignSelf: 'flex-end', alignItems: 'flex-end', marginTop: -10, paddingHorizontal: width * .15 }}>
+
+                                    <TabBarIcon name='md-information-circle' focused={true} />
+                                </View>
+                            </TouchableOpacity>
+                        }
+                    />
                 </View>
             </View>
-            <View style={{ flex: 4 }}>
-                <FlatList
-                    style={{ width }}
-                    data={ProductLists}
-                    keyExtractor={_keyExtractor}
-                    renderItem={({ item, index }) =>
-                        <TouchableOpacity style={{ flexDirection: 'row', width, margin: 10 }}>
-                            <View style={{ marginHorizontal: 10 }} >
-                                <FontAwesome name="certificate" size={20} />
-                            </View>
-                            <View >
-                                <Text >{item.name}</Text>
-                            </View>
-                        </TouchableOpacity>
-                    }
-                />
-            </View>
-        </View>
-    );
+        );
+    }
 }
-
-ProductListScreen.navigationOptions = {
-    header: null,
-};
-
-
-
 const styles = {
     container: {
         flex: 1,
