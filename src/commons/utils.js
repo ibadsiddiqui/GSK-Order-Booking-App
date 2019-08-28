9f585571bd17e683cc46a66230291d71c655c293
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-29 02:16:45 +0500

    --Commit: add and reduce item added

--- src/commons/utils.js
+++ src/commons/utils.js
@@ -2,12 +2,29 @@ export const _keyExtractor = (item, index) => {
     return item.id;
 };
 
-export const addItem = (array, item) => {
-    return array.map((_item, index) => {
-        _item.id === item.id ? {
-            ...item,
+export const addItem = (array, id) => {
+    return array.map((_item) =>
+        _item.id === id ? {
+            ..._item,
             qty: _item.qty + 1,
         }
             : { ..._item }
-    });
+    );
+}
+
+export const reduceItem = (array, id) => {
+    return array.map((_item) => {
+        if (_item.id === id) {
+            if (_item.qty >= 1)
+                return {
+                    ..._item,
+                    qty: _item.qty - 1,
+                }
+            else return {
+                ..._item,
+                qty: 0,
+            }
+        }
+        else return { ..._item }
+    })
 }
