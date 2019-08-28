670f3913f5656776650c160a661c626841e1a963
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-29 02:17:08 +0500

    --Commit: reduce item count added

--- src/redux/actions/ProductsActions.js
+++ src/redux/actions/ProductsActions.js
@@ -1,8 +1,15 @@
-import { RESET_PRODUCTS_FOR_ORDER, ADD_ITEM } from "../types";
+import { RESET_PRODUCTS_FOR_ORDER, ADD_ITEM, REDUCE_ITEM } from "../types";
 
 export const addProductCount = (id) => {
     return {
         type: ADD_ITEM,
+        payload: id
+    }
+}
+
+export const reduceProductCount = (id) => {
+    return {
+        type: REDUCE_ITEM,
         payload: id
     }
 }
