23e7d3402d2490efb029e214ffb6ca1e1ad17a84
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-28 15:16:05 +0500

    --Commit: date picker added

--- src/screens/Dashboard/OrderTab/AddOrder/index.js
+++ src/screens/Dashboard/OrderTab/AddOrder/index.js
@@ -1,23 +1,46 @@
-import { AntDesign, Feather, Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { AntDesign, Entypo, Feather, Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import React from 'react';
-import { Dimensions, Text, View } from 'react-native';
+import { DatePickerAndroid, Dimensions, Text, View } from 'react-native';
 import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
+import { connect } from "react-redux";
 import TabBarIcon from '../../../../components/TabBarIcon';
 import Colors from '../../../../constants/Colors';
 import Layout from '../../../../constants/Layout';
+import { mapDispatchToProps, mapStateToProps } from "../../../../redux/dispatcher";
 const { width } = Dimensions.get('window')
 
-export default class AddNewOrder extends React.Component {
+class AddNewOrder extends React.Component {
     static navigationOptions = {
         title: 'Create Order',
         headerBackground: true
     };
 
-    onPress(props, productInfo) {
-        return props.navigation.navigate('ProductInfo', { productInfo: productInfo })
+    setIssueDate = async (key) => {
+        try {
+
+            const { action, year, month, day } = await DatePickerAndroid.open({
+                date: new Date(),
+                minDate: new Date(),
+            });
+            switch (key) {
+                case 'issue':
+                    if (action !== DatePickerAndroid.dismissedAction)
+                        this.props.setOrderIssueDate(new Date(year, month, day).toLocaleDateString());
+                    break;
+                case 'delivery':
+                    if (action !== DatePickerAndroid.dismissedAction)
+                        this.props.setOrderDeliveryDate(new Date(year, month, day).toLocaleDateString());
+                    break;
+                default:
+                    break;
+            }
+        } catch ({ code, message }) {
+            console.warn('Cannot open date picker', message);
+        }
     }
 
     render() {
+        const { orderIssueDate, orderDeliveryDate } = this.props;
         return (
             <View style={styles.container}>
                 <View style={[Layout.table,]}>
@@ -30,7 +53,9 @@ export default class AddNewOrder extends React.Component {
                                 <Text style={{ fontSize: 14 }}>Issue Date: </Text>
                             </View>
                             <View style={[Layout.tableCell, { alignItems: 'flex-end', padding: 6, paddingRight: 20 }]}>
-                                <Text>{new Date().toLocaleDateString()}</Text>
+                                <TouchableOpacity onPress={() => this.setIssueDate('issue')}>
+                                    <Text>{orderIssueDate}</Text>
+                                </TouchableOpacity>
                             </View>
                         </View>
                         <View style={[Layout.tableRow, { marginTop: 20 }]}>
@@ -41,11 +66,14 @@ export default class AddNewOrder extends React.Component {
                                 <Text style={{ fontSize: 14 }}>Delivery Date: </Text>
                             </View>
                             <View style={[Layout.tableCell, { alignItems: 'flex-end', padding: 6, paddingRight: 20 }]}>
-                                <Text>{new Date().toLocaleDateString()}</Text>
+                                <TouchableOpacity onPress={() => this.setIssueDate('delivery')}>
+                                    <Text>{orderDeliveryDate}</Text>
+                                </TouchableOpacity>
+
                             </View>
                         </View>
                         <View style={[Layout.tableRow, { marginTop: 20 }]}>
-                            <View style={[Layout.tableCell, { flex: 0.4, paddingLeft: 12.5 }]}>
+                            <View style={[Layout.tableCell, { flex: 0.425, paddingLeft: 12.5 }]}>
                                 <MaterialCommunityIcons name="cards" color={Colors.tintColor} size={30} />
                             </View>
                             <View style={[Layout.tableCell, { flex: 2, alignSelf: 'flex-start', padding: 2 }]}>
@@ -56,10 +84,10 @@ export default class AddNewOrder extends React.Component {
                             </View>
                         </View>
                         <View style={[Layout.tableRow, { marginTop: 20 }]}>
-                            <View style={[Layout.tableCell, { flex: 0.4, paddingLeft: 12.5 }]}>
+                            <View style={[Layout.tableCell, { flex: 0.35, paddingLeft: 12.5 }]}>
                                 <MaterialCommunityIcons name="cards" color={Colors.tintColor} size={30} />
                             </View>
-                            <View style={[Layout.tableCell, { flex: 2, alignSelf: 'flex-start', padding: 2 }]}>
+                            <View style={[Layout.tableCell, { flex: 2, alignSelf: 'flex-start', padding: 5 }]}>
                                 <Text style={{ fontSize: 14 }}>Add Picture: </Text>
                             </View>
                             <View style={[Layout.tableCell, { flex: 0.2, alignItems: 'flex-end', padding: 2.5, paddingRight: 20 }]}>
@@ -81,11 +109,11 @@ export default class AddNewOrder extends React.Component {
                             <View style={[Layout.tableCell, { flex: 0.4, paddingLeft: 12.5 }]}>
                                 <MaterialCommunityIcons name="cards" color={Colors.tintColor} size={30} />
                             </View>
-                            <View style={[Layout.tableCell, { flex: 2, alignSelf: 'flex-start', padding: 2 }]}>
+                            <View style={[Layout.tableCell, { flex: 2, alignSelf: 'flex-start', padding: 5 }]}>
                                 <Text style={{ fontSize: 14 }}>Pick Location: </Text>
                             </View>
                             <View style={[Layout.tableCell, { alignItems: 'flex-end', padding: 2.5, paddingRight: 20 }]}>
-                                <Feather name="chevrons-right" color={Colors.tintColor} size={30} />
+                                <Entypo name="location-pin" color={Colors.tintColor} size={30} />
                             </View>
                         </View>
                     </ScrollView>
@@ -107,7 +135,6 @@ export default class AddNewOrder extends React.Component {
     }
 }
 
-AddNewOrder
 
 const styles = {
     container: {
@@ -116,3 +143,5 @@ const styles = {
         ...Layout.table
     },
 };
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddNewOrder);
