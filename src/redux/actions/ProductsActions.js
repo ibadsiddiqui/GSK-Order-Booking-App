fb0f5d1efdeada9d1a2be56911686aad2195c84a
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-29 01:14:48 +0500

    --Commit: products action added

--- src/redux/actions/ProductsActions.js
+++ src/redux/actions/ProductsActions.js
@@ -1,1 +1,14 @@
-// export const 
+import { RESET_PRODUCTS_FOR_ORDER, ADD_ITEM } from "../types";
+
+export const addProductCount = (id) => {
+    return {
+        type: ADD_ITEM,
+        payload: id
+    }
+}
+
+export const resetProductList = () => {
+    return {
+        type: RESET_PRODUCTS_FOR_ORDER
+    }
+}
