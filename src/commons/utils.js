d8a9b04ca2a051b4b2bf1b66417951705071b5fa
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-29 01:14:03 +0500

    --commit: util added

--- src/commons/utils.js
+++ src/commons/utils.js
@@ -1,3 +1,13 @@
 export const _keyExtractor = (item, index) => {
     return item.id;
 };
+
+export const addItem = (array, item) => {
+    return array.map((_item, index) => {
+        _item.id === item.id ? {
+            ...item,
+            qty: _item.qty + 1,
+        }
+            : { ..._item }
+    });
+}
