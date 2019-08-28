637f47febe14614b711a73d620475dd7313d185b
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-29 01:14:39 +0500

    --Commit: dispatchers added for products reducer

--- src/redux/dispatcher/index.js
+++ src/redux/dispatcher/index.js
@@ -1,7 +1,13 @@
 import { AddOrderDeliveryDate, AddOrderGeoLocation, AddOrderIssueDate, AddOrderProduct, AddOrderShopPicture, CreateOrder, AddOrderProducts, ResetOrderProducts } from "../actions/OrdersActions";
+import { resetProductList, addProductCount } from "../actions/ProductsActions";
 
 export const mapStateToProps = (state) => {
     return {
+        // from products reducer
+        productLists: state.product.ProductLists,
+
+        // from orders reducer
+
         orderIssueDate: state.orders.orderIssueDate,
         orderDeliveryDate: state.orders.orderDeliveryDate,
         selectedProducts: state.orders.selectedProducts,
@@ -25,5 +31,10 @@ export const mapDispatchToProps = (dispatch) => {
         addOrderGeoLocation: (location) => dispatch(AddOrderGeoLocation(location)),
         addOrderShopPicture: pic => dispatch(AddOrderShopPicture(pic)),
         createOrder: (order) => dispatch(CreateOrder(order)),
+
+        // Products Dispatchers
+        resetProductList: () => dispatch(resetProductList()),
+        addProductCount: (id) => dispatch(addProductCount(id))
+
     }
 }
