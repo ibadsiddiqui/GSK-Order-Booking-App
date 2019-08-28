f155019ddba34f4ea87a1b1cea617d64ff8ded77
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-29 02:42:04 +0500

    --Commit: date picker added

--- src/helpers/DateHelpers.js
+++ src/helpers/DateHelpers.js
@@ -1,7 +1,22 @@
 import moment from "moment";
+import { DatePickerAndroid } from 'react-native';
 
 export function slicingMomentDateUsingAt(date) {
     if (moment(date).calendar().includes(" at ")) {
         return moment(date).calendar().slice(0, moment(date).calendar().indexOf(" at "))
     } else return moment(date).calendar()
+}
+
+export const pickDateForOrder = async () => {
+    try {
+        const { action, year, month, day } = await DatePickerAndroid.open({
+            date: new Date(),
+            minDate: new Date(),
+        });
+        if (action !== DatePickerAndroid.dismissedAction)
+            return new Date(year, month, day).toLocaleDateString();
+        else return { cancelled: true }
+    } catch ({ code, message }) {
+        console.warn('Cannot open date picker', message);
+    }
 }
