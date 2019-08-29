d81bc1cc4b17cb2c7d972cbdc1910451bc047f76
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-29 17:42:34 +0500

    --Commit: map view screen added

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,4 +1,5 @@
-import { createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation';
+import { createAppContainer, createStackNavigator, createSwitchNavigator } from 'react-navigation';
+import MapViewScreen from '../screens/MapView';
 import ProductInfo from '../screens/ProductInfo';
 import AddProductToOrderScreen from './../screens/AddProductToOder';
 import MainTabNavigator from './MainTabNavigator';
@@ -7,8 +8,9 @@ export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+    MapView: MapViewScreen,
     Main: MainTabNavigator,
     ProductInfo: createStackNavigator({ screen: ProductInfo }),
-    AddProductToOrder: createStackNavigator({ screen: AddProductToOrderScreen })
+    AddProductToOrder: createStackNavigator({ screen: AddProductToOrderScreen }),
   })
 );
