0f8519b7b81da7a314109a277aa84710f3e1df16
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-29 23:44:53 +0500

    --commit: map view updated with location and draggable marker

--- src/constants/Permissions.js
+++ src/constants/Permissions.js
@@ -1,4 +1,5 @@
 import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location';
 
 export async function takeCameraPermissionAsync() {
     const { status: cameraPerm } = await Permissions.askAsync(
@@ -17,4 +18,12 @@ export async function takeCameraRollPermissionAsync() {
 export async function getLocationPermission() {
     const { status: locationPerm } = await Permissions.askAsync(Permissions.LOCATION);
     return locationPerm;
+}
+
+export async function checkLocationService() {
+    return await Location.hasServicesEnabledAsync()
+}
+
+export async function requestLocationService() {
+    return await Location.requestPermissionsAsync()
 }
--- src/screens/MapView/index.js
+++ src/screens/MapView/index.js
@@ -5,28 +5,29 @@ import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import MapView, { Marker } from "react-native-maps";
 import { connect } from "react-redux";
-import { getLocationPermission } from '../../constants/Permissions';
+import { getLocationPermission, checkLocationService, requestLocationService } from '../../constants/Permissions';
 import { mapDispatchToProps, mapStateToProps } from "../../redux/dispatcher";
+import Colors from './../../constants/Colors';
 const { width } = Dimensions.get('window')
+
+const LATITUDEDELTA = 0.0922;
+const LONGITUDEDELTA = 0.0421
 
 class MapViewScreen extends React.Component {
     static navigationOptions = {
         header: null
     };
+    // map = null;
     constructor() {
         super();
         this.state = {
-            mapView: false,
+            mapView: true,
             region: {
                 latitude: 37.78825,
                 longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
+                latitudeDelta: LATITUDEDELTA,
+                longitudeDelta: LONGITUDEDELTA,
             },
-            shopLocation: {
-                latitude: 37.78825,
-                longitude: -122.4324,
-            }
         }
     }
 
@@ -34,29 +35,58 @@ class MapViewScreen extends React.Component {
 
     _getLocationAsync = async () => {
         let status = await getLocationPermission();
-        if (status !== 'granted') {
-            this.setState({
-                errorMessage: 'Permission to access location was denied',
-            });
+        let serviceStatus = await checkLocationService()
+        let location;
+        if (serviceStatus) {
+            location = await Location.getCurrentPositionAsync({});
+        } else {
+            const status = await requestLocationService();
+            if (status === "granted" || status) {
+                location = await Location.getCurrentPositionAsync({});
+            } else {
+                this.setState({
+                    mapView: false,
+                });
+                return;
+            }
         }
-
-        let location = await Location.getCurrentPositionAsync({});
+        if (status !== 'granted')
+            this.setState({
+                mapView: false,
+            });
         this.setState({
             region: {
                 ...location.coords,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
+                latitudeDelta: LATITUDEDELTA,
+                longitudeDelta: LONGITUDEDELTA,
             },
-            shopLocation: {
-                latitude: location.coords.latitude,
-                longitude: location.coords.latitude,
-            }
         });
+        this.map.animateToRegion({ latitude: this.state.region.latitude, longitude: this.state.region.longitude })
     };
+
+    setShopLocation = (e) => this.setState({
+        region: {
+            latitude: e.nativeEvent.coordinate.latitude,
+            longitude: e.nativeEvent.coordinate.longitude,
+            latitudeDelta: LATITUDEDELTA,
+            longitudeDelta: LONGITUDEDELTA,
+        }
+    })
 
     toggleMapView = (status) => this.setState({ mapView: status })
 
+    addLocation = async () => {
+        const { latitude, longitude } = this.state.region;
+        let address = await Location.reverseGeocodeAsync({ latitude, longitude });
+        console.log({
+            ...address,
+            ...this.state.region
+        });
+        return
+    }
+
     render() {
+
         if (!this.state.mapView)
             return (
                 <View style={{ flex: 1 }}>
@@ -88,18 +118,34 @@ class MapViewScreen extends React.Component {
                 </View >
             );
         else return (
-            <MapView style={{ flex: 1, ...StyleSheet.absoluteFillObject }}>
-
-                <Marker
-                    coordinate={this.state.shopLocation}
-                    onSelect={e => console.log('onSelect', e)}
-                    onDrag={e => console.log('onDrag', e)}
-                    onDragStart={e => console.log('onDragStart', e)}
-                    onDragEnd={e => console.log('onDragEnd', e)}
-                    onPress={e => console.log('onPress', e)}
-                    draggable
-                />
-            </MapView>
+            <View style={{ flex: 1, ...StyleSheet.absoluteFill }}>
+                <MapView style={{ flex: 1, ...StyleSheet.absoluteFillObject }}
+                    ref={ref => this.map = ref}
+                >
+                    <Marker draggable
+                        coordinate={this.state.region}
+                        // onSelect={this.setShopLocation}
+                        // onDragStart={this.setShopLocation}
+                        onDragEnd={this.setShopLocation}
+                    // onPress={this.setShopLocation}
+                    // onDrag={this.setShopLocation}
+                    />
+                </MapView>
+                <View style={{ position: 'absolute', bottom: 20, alignSelf: 'center' }}>
+                    <TouchableOpacity onPress={this.addLocation}>
+                        <View style={{
+                            justifyContent: 'center',
+                            alignSelf: 'center',
+                            height: 50,
+                            width: 130,
+                            backgroundColor: Colors.noticeBackground,
+                            borderRadius: 25
+                        }}>
+                            <Text style={{ fontSize: 16, color: Colors.white, textAlign: 'center' }}>Select Location</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
         )
     }
 }
