2a8fa7a92bee1be74896f2d448816c6ca2bbb829
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-31 23:05:31 +0500

    --Commit: controller updated for reset order details

--- src/controllers/OrdersController.js
+++ src/controllers/OrdersController.js
@@ -3,7 +3,16 @@ export default class OrdersController {
 
     }
 
-    AddProductsForOrdering(props) {
-        return props.resetOrderProducts();
+    static resetOrderdetails(props) {
+        props.resetProductList();                                       // reset product list
+        props.addProductToOrder(new Array());                           // reset product selection
+        props.addAttachmentToOrder("");                                 // reset attachment 
+        props.addOrderGeoLocation(new Object());                        // reset location
+        props.addOrderDiscount(0);                                      // reset the discount rate
+        props.resetOrderShopPicture()                                   // reset picture taken,
+        props.setOrderIssueDate(new Date().toLocaleDateString());       // reset order issue date
+        props.setOrderDeliveryDate(new Date().toLocaleDateString());    // reset delivery date
+        props.addOrderShopDetails(new Object());                        // reset shop detail for delivery
+
     }
 } 
