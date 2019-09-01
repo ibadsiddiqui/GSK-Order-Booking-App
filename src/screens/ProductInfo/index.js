3f8953d822720f12a7cb333a9a8781350f66f766
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-09-01 22:28:11 +0500

    --Commit: styling updated

--- src/screens/ProductInfo/index.js
+++ src/screens/ProductInfo/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { BackHandler, Image, SectionList, StyleSheet, Text, View } from 'react-native';
+import { BackHandler, SectionList, StyleSheet, Text } from 'react-native';
+import SectionContent from '../../components/common/SectionListComponents/SectionContent';
+import Color from '../../components/common/SectionListComponents/Item';
+import ListHeader from '../../components/common/SectionListComponents/ListHeader';
+import SectionHeader from '../../components/common/SectionListComponents/SectionHeader';
 
 export default class ProductInfo extends React.Component {
     componentDidMount() {
         this.backhandler = BackHandler.addEventListener('hardwareBackPress', () => this.props.navigation.navigate('Main'))
     }
 
-    componentWillUnmount() {
-        this.backhandler.remove();
-    }
+    componentWillUnmount = () => this.backhandler.remove();
 
     render() {
         const productInfo = this.props.navigation.getParam("productInfo", {});
@@ -16,6 +18,7 @@ export default class ProductInfo extends React.Component {
             { data: [{ value: productInfo.id }], title: 'Product ID:' },
             { data: [{ value: productInfo.name }], title: 'Product Name:' },
             { data: [{ value: productInfo.tradePrice }], title: 'Trade Price:' },
+            // { data: [{ value: "" }], title: 'Discount Avalable:' },
             { data: [{ value: productInfo.mrp }], title: 'MRP:' },
         ]
         return (
@@ -48,116 +51,14 @@ export default class ProductInfo extends React.Component {
     };
 }
 
-const ListHeader = () => {
-    return (
-        <View style={styles.titleContainer}>
-            <View style={styles.titleIconContainer}>
-                <AppIconPreview />
-            </View>
-
-            <View style={styles.titleTextContainer}>
-                <Text style={styles.nameText} numberOfLines={1}>
-                    GSK Product
-                </Text>
-            </View>
-        </View>
-    );
-};
-
-const SectionHeader = ({ title }) => {
-    return (
-        <View style={styles.sectionHeaderContainer}>
-            <Text style={styles.sectionHeaderText}>{title}</Text>
-        </View>
-    );
-};
-
-const SectionContent = props => {
-    return <View style={styles.sectionContentContainer}>{props.children}</View>;
-};
-
-const AppIconPreview = () => {
-    return <Image source={require('./../../assets/images/robot-prod.png')} style={{ width: 64, height: 64 }} resizeMode="cover" />;
-};
-
-const Color = ({ value }) => {
-    if (!value) {
-        return <View />;
-    } else {
-        return (
-            <View style={styles.colorContainer}>
-                <View style={[styles.colorPreview, { backgroundColor: value }]} />
-                <View style={styles.colorTextContainer}>
-                    <Text style={styles.sectionContentText}>{value}</Text>
-                </View>
-            </View>
-        );
-    }
-};
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
         paddingTop: 10
     },
-    titleContainer: {
-        paddingHorizontal: 15,
-        paddingTop: 20,
-        paddingBottom: 15,
-        flexDirection: 'row',
-    },
-    titleIconContainer: {
-        marginRight: 15,
-        paddingTop: 2,
-    },
-    sectionHeaderContainer: {
-        backgroundColor: '#fbfbfb',
-        paddingVertical: 8,
-        paddingHorizontal: 15,
-        borderWidth: StyleSheet.hairlineWidth,
-        borderColor: '#ededed',
-    },
-    sectionHeaderText: {
-        fontSize: 15,
-        fontWeight: 'bold'
-    },
-    sectionContentContainer: {
-        paddingTop: 8,
-        paddingBottom: 12,
-        paddingHorizontal: 15,
-    },
-    sectionContentText: {
-        color: '#808080',
-        fontSize: 14,
-    },
     nameText: {
         fontWeight: '600',
         fontSize: 20,
-    },
-    slugText: {
-        color: '#a39f9f',
-        fontSize: 14,
-        backgroundColor: 'transparent',
-    },
-    descriptionText: {
-        fontSize: 14,
-        marginTop: 6,
-        color: '#4d4d4d',
-    },
-    colorContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    colorPreview: {
-        width: 17,
-        height: 17,
-        borderRadius: 2,
-        marginRight: 6,
-        borderWidth: StyleSheet.hairlineWidth,
-        borderColor: '#ddd',
-    },
-    colorTextContainer: {
-        flex: 1,
     },
 });
--- src/screens/ShopInfo/index.js
+++ src/screens/ShopInfo/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { BackHandler, Image, SectionList, StyleSheet, Text, View } from 'react-native';
+import { BackHandler, SectionList, StyleSheet, Text } from 'react-native';
+import SectionContent from '../../components/common/SectionListComponents/SectionContent';
+import SectionHeader from '../../components/common/SectionListComponents/SectionHeader';
+import ListHeader from '../../components/common/SectionListComponents/ListHeader';
 
 export default class ShopInfoScreen extends React.Component {
     componentDidMount() {
@@ -52,73 +55,11 @@ export default class ShopInfoScreen extends React.Component {
     }
 }
 
-const ListHeader = () => {
-    return (
-        <View style={styles.titleContainer}>
-            <View style={styles.titleIconContainer}>
-                <AppIconPreview />
-            </View>
-
-            <View style={styles.titleTextContainer}>
-                <Text style={styles.nameText} numberOfLines={1}>
-                    Shop Details:
-                </Text>
-            </View>
-        </View>
-    );
-};
-
-const SectionHeader = ({ title }) => {
-    return (
-        <View style={styles.sectionHeaderContainer}>
-            <Text style={styles.sectionHeaderText}>{title}</Text>
-        </View>
-    );
-};
-
-const SectionContent = props => {
-    return <View style={styles.sectionContentContainer}>{props.children}</View>;
-};
-
-const AppIconPreview = () => {
-    return <Image source={require('./../../assets/images/robot-prod.png')} style={{ width: 64, height: 64 }} resizeMode="cover" />;
-};
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
         paddingTop: 10
-    },
-    titleContainer: {
-        paddingHorizontal: 15,
-        paddingTop: 20,
-        paddingBottom: 15,
-        flexDirection: 'row',
-    },
-    titleIconContainer: {
-        marginRight: 15,
-        paddingTop: 2,
-    },
-    sectionHeaderContainer: {
-        backgroundColor: '#fbfbfb',
-        paddingVertical: 8,
-        paddingHorizontal: 15,
-        borderWidth: StyleSheet.hairlineWidth,
-        borderColor: '#ededed',
-    },
-    sectionHeaderText: {
-        fontSize: 15,
-        fontWeight: 'bold'
-    },
-    sectionContentContainer: {
-        paddingTop: 8,
-        paddingBottom: 12,
-        paddingHorizontal: 15,
-    },
-    sectionContentText: {
-        color: '#808080',
-        fontSize: 14,
     },
     nameText: {
         fontWeight: '600',
