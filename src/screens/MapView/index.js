1cd419545726d42297cf90ee2513ce5232c4478e
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-09-11 17:25:45 +0500

    --Commit: map api added

--- src/screens/MapView/index.js
+++ src/screens/MapView/index.js
@@ -12,6 +12,7 @@ import Colors from './../../constants/Colors';
 const { width } = Dimensions.get('window')
 const LATITUDEDELTA = 0.0922;
 const LONGITUDEDELTA = 0.0421
+// var Polyline = require('@mapbox/polyline');
 
 class MapViewScreen extends React.Component {
     static navigationOptions = {
@@ -32,8 +33,34 @@ class MapViewScreen extends React.Component {
     }
 
     async componentDidMount() {
-        await this._getLocationAsync()
+        await this._getLocationAsync();
+        // fetch directions and decode polylines
+        // this.getDirections("24.863633780273133,67.07325367256999", `${this.state.region.latitude},${this.state.region.longitude}`)
     }
+
+    // async getDirections(startLoc, destinationLoc) {
+    //     try {
+    //         let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${destinationLoc}&key=AIzaSyDgVapRiIw_sh1zoxcouyP_Fvlbqk35H5Q`)
+    //         // https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=YOUR_API_KEY
+    //         let respJson = await resp.json();
+    //         console.log('====================================');
+    //         console.log(respJson);
+    //         console.log('====================================');
+    //         let points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+    //         let coords = points.map((point, index) => {
+    //             return {
+    //                 latitude: point[0],
+    //                 longitude: point[1]
+    //             }
+    //         })
+    //         this.setState({ coords: coords })
+    //         return coords
+    //     } catch (error) {
+    //         alert(error)
+    //         return error
+    //     }
+    // }
+
 
     _getLocationAsync = async () => {
         await getLocationPermission();
